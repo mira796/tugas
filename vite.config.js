81dff17a3b5a9ae4e977fded5a08b1b88dd8802d
mira796 <mirahutasoit3@gmail.com>
2025-07-05 22:15:03 +0700

    Prepare for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // atur base ke root agar routing dan asset jalan dengan benar
-});
+  base: '/',
+})
